ef1474e033f3330bc617afd92356fbf9ef0db9a2
Japhet1 <elompassah81@gmail.com>
2024-07-19 11:25:31 +0000

    nodemon install

---
+++ Day_6/spin_words.js
@@ -0,0 +1,15 @@
+"use strict";
+/*Write a function that takes in a string of one or more words, and returns the same string,
+but with all words that have five or more letters reversed (Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.**/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.spinWords = spinWords;
+function spinWords(words) {
+    //TODO Have fun :)
+    const splitWord = words.split(" ");
+    const filterWord = splitWord.map(str => str.length >= 5 ? str.split('').reverse().join('') : str).join(" ");
+    console.log(filterWord);
+    return filterWord;
+}
+spinWords("code challenge actually going on well, thanks for asking");
